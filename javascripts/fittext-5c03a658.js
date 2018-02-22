d340c67bcec38c96bb3180e06064537c155ca8af
Mikhail Zholobov <legal90@gmail.com>
2018-02-22 09:42:59 +0100

    Automated commit at 2018-02-22 08:42:59 UTC by middleman-deploy 1.0.0

--- javascripts/fittext-5c03a658.js
+++ javascripts/fittext-5c03a658.js
@@ -7,4 +7,4 @@
 *
 * Date: Thu May 05 14:23:00 2011 -0600
 */
-!function(e){e.fn.fitText=function(t,n){var i=t||1,r=e.extend({minFontSize:Number.NEGATIVE_INFINITY,maxFontSize:Number.POSITIVE_INFINITY},n);return this.each(function(){var t=e(this),n=function(){t.css("font-size",Math.max(Math.min(t.width()/(10*i),parseFloat(r.maxFontSize)),parseFloat(r.minFontSize)))};n(),e(window).on("resize",n)})}}(jQuery);
+!function(e){e.fn.fitText=function(t,n){var r=t||1,i=e.extend({minFontSize:Number.NEGATIVE_INFINITY,maxFontSize:Number.POSITIVE_INFINITY},n);return this.each(function(){var t=e(this),n=function(){t.css("font-size",Math.max(Math.min(t.width()/(10*r),parseFloat(i.maxFontSize)),parseFloat(i.minFontSize)))};n(),e(window).on("resize",n)})}}(jQuery);
